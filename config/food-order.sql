f58bfd8475580892ba470dc8a87021a1461380d8
jkmanait <jkmanait@gmail.com>
2024-06-03 16:04:46 +0800

    changed sql file

--- config/food-order.sql
+++ config/food-order.sql
@@ -1,13 +1,14 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.1
+-- version 4.8.5
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 03, 2024 at 07:36 AM
--- Server version: 10.4.28-MariaDB
--- PHP Version: 8.0.28
+-- Generation Time: Jun 03, 2024 at 10:04 AM
+-- Server version: 10.1.38-MariaDB
+-- PHP Version: 7.3.2
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -32,7 +33,7 @@ CREATE TABLE `tbl_admin` (
   `full_name` varchar(100) NOT NULL,
   `username` varchar(100) NOT NULL,
   `password` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -48,15 +49,19 @@ CREATE TABLE `tbl_food` (
   `image_name` varchar(255) NOT NULL,
   `featured` varchar(10) NOT NULL,
   `active` varchar(10) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `tbl_food`
 --
 
 INSERT INTO `tbl_food` (`id`, `title`, `description`, `price`, `image_name`, `featured`, `active`) VALUES
-(25, 'Fish Ball', 'Fish Ball', 10.00, 'Food-Name-37.PNG', 'Yes', 'Yes'),
-(26, 'Proven', 'Proven', 5.00, 'Food-Name-9813.PNG', 'Yes', 'Yes');
+(31, 'proven', 'proven', '5.00', 'proven.PNG', 'Yes', 'Yes'),
+(32, 'kwek-kwek', 'kwek-kwek', '12.00', 'kwek kwek.PNG', 'Yes', 'Yes'),
+(33, 'Fish Ball', 'Fish Ball', '12.00', 'fishball.PNG', 'Yes', 'Yes'),
+(34, 'Buko Juice', 'Buko Juice', '10.00', 'bj.PNG', 'Yes', 'Yes'),
+(35, 'Banana Q', 'Banana Q', '7.00', 'banana q.PNG', 'Yes', 'Yes'),
+(36, 'Isaw', 'Isaw', '8.00', 'isaw.PNG', 'Yes', 'Yes');
 
 -- --------------------------------------------------------
 
@@ -76,7 +81,14 @@ CREATE TABLE `tbl_order` (
   `customer_contact` varchar(20) NOT NULL,
   `customer_email` varchar(150) NOT NULL,
   `customer_address` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `tbl_order`
+--
+
+INSERT INTO `tbl_order` (`id`, `food`, `price`, `qty`, `total`, `order_date`, `status`, `customer_name`, `customer_contact`, `customer_email`, `customer_address`) VALUES
+(15, 'proven', '5.00', 10, '50.00', '2024-06-03 10:00:30', 'Ordered', 'Reggie Madrjanon', '0909090909', 'ReggieMadrjanon@gmail.com', 'didto');
 
 --
 -- Indexes for dumped tables
@@ -114,13 +126,13 @@ ALTER TABLE `tbl_admin`
 -- AUTO_INCREMENT for table `tbl_food`
 --
 ALTER TABLE `tbl_food`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=27;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=38;
 
 --
 -- AUTO_INCREMENT for table `tbl_order`
 --
 ALTER TABLE `tbl_order`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
